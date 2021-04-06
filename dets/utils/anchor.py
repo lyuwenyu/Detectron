8c520e07d26b71b5de57a1fe84b8ae1122257c95
lyuwenyu <wenyu.lyu@gmail.com>
2021-04-06 15:21:51 +0800

    add prior anchors

--- dets/utils/anchor.py
+++ dets/utils/anchor.py
@@ -7,63 +7,80 @@ import torch
 
 
 class PriorBox(object):
-    def __init__(self, ):
+    def __init__(self, img_size=640, strides=8, ):
+        '''
+        img_size [w, h]
+        stride [8, 16, 32]
+
+        '''
+        if not isinstance(img_size, (list, tuple)):
+            img_size = (img_size, img_size)
         
-        self.img_dim = 320
-        self.strides = [8, 16, 32]
-        self.grids = [int(self.img_dim / s) for s in self.strides]
+        if not isinstance(strides, (list, tuple)):
+            strides = (strides, )
+
+        self.img_size = img_size
+        self.strides = strides
 
         # anchor size
-        self.base_sizes = [60, 120, 180]
-        self.area_rarios = [[2, ], [2, ], [2, ]]
-        self.aspect_rarios = [[2, ], [2, 3], [2, 3]]
+        self.base_sizes = [(60, 60), (120, 120), (180, 180)]
+        self.aspect_rarios = [(2, ), (2, ), (2, )]
+        self.area_rarios = [(2, ), (2, ), (2, )]
+
 
     def __call__(self, ):
         '''
-        generate prior bbox, [cx, cy, w, h] and normalized
+        generate prior bbox, [cx, cy, w, h] and normalized, respect to orgin image
         return: torch.Tensor
         '''
         anchors = []
-        for i, grid in enumerate(self.grids):
+
+        for i, s in enumerate(self.strides):
             
-            # center_x center_y
-            yy, xx = np.meshgrid(range(grid), range(grid))
-            # yy, xx = torch.meshgrid((torch.arange(grid), torch.arange(grid)))
-            cx = (xx + 0.5) / grid
-            cy = (yy + 0.5) / grid
-        
-            # width height
+            grid_w = int(self.img_size[0] / s)
+            grid_h = int(self.img_size[1] / s)
+
+            gi, gj = np.meshgrid(range(grid_w), range(grid_h))
+
+            cx = (gi + 0.5) / grid_w
+            cy = (gj + 0.5) / grid_h
+
             whs = []
-            base = self.base_sizes[i] / self.img_dim
-            whs += [[base, base]]
+
+            base_w = self.base_sizes[i][0] / self.img_size[0]
+            base_h = self.base_sizes[i][1] / self.img_size[1]
+
+            whs.append([base_w, base_h])
 
             for ar in self.aspect_rarios[i]:
-                whs += [[base * math.sqrt(ar), base / math.sqrt(ar)]]
-                whs += [[base / math.sqrt(ar), base * math.sqrt(ar)]]
+                whs.append( [base_w * math.sqrt(ar), base_h / math.sqrt(ar)] )
+                whs.append( [base_w / math.sqrt(ar), base_h * math.sqrt(ar)] )
             
             for ar in self.area_rarios[i]:
-                whs += [[base * math.sqrt(ar), base * math.sqrt(ar)]]
-                whs += [[base / math.sqrt(ar), base / math.sqrt(ar)]]
+                whs += [[base_w * math.sqrt(ar), base_h * math.sqrt(ar)]]
+                whs += [[base_w / math.sqrt(ar), base_h / math.sqrt(ar)]]
 
             whs = np.array(whs)
-            # whs = torch.from_numpy(np.array(whs))
-            # print(len(whs))
 
-            # n = xx.shape[0] * xx.shape[1]
             priors = np.zeros((np.prod(cx.shape), len(whs), 4))
-            # print(priors.shape)
-            # priors = torch.zeros((n, len(whs), 4))
+
             priors[:, :, 0] = cx.reshape(-1, 1)
             priors[:, :, 1] = cy.reshape(-1, 1)
-            priors[:, :, 2:] = whs
+            priors[:, :, 2:] = whs.reshape(-1, 2)
 
             anchors += [priors.reshape(-1, 4)]
-            # print(np.prod(cx.shape) * len(whs))
 
         anchors = np.concatenate(anchors, axis=0)
-        # anchors = torch.cat(anchors, dim=0)
-        # print(anchors.shape)
-        anchors = torch.from_numpy(anchors).to(dtype=torch.float)
-        # anchors.clamp_(max=1., min=0.)
-        # print('anchors: ', anchors.shape)
+        # anchors = torch.from_numpy(anchors).to(dtype=torch.float)
+
         return anchors
+
+
+
+if __name__ == '__main__':
+
+
+    bbox = PriorBox(img_size=(640, 512), strides=(8, 16))()
+
+    print(bbox.shape)
+    print(bbox[-10:])
