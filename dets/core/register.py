f686ffbf35252ea5eef6542c30d0c2c36250c97d
lyuwenyu <wenyu.lyu@gmail.com>
2021-04-07 13:53:53 +0800

    add register

--- dets/core/register.py
+++ dets/core/register.py
@@ -1,2 +1,26 @@
 from collections import OrderedDict
 
+
+import inspect
+import importlib
+
+
+WORKSPACE = {}
+
+
+def register(clas):
+    '''register
+    '''    
+    assert clas.__name__ not in WORKSPACE, ''
+
+    WORKSPACE[clas.__name__] = clas
+    # importlib.import_module(clas.__module__)
+
+    return clas 
+
+
+def create(name):
+    '''create
+    '''
+    pass
+
